880537d833e1694c6c43d1a046c255a8b53e3cf9
eshref04 <esrefsefiyev@gmail.com>
2024-05-10 13:09:16 +0400

    mvc 1

---
+++ WebApplication7/Areas/adminkanermin/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication7.Areas.adminkanermin.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("adminkanermin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
